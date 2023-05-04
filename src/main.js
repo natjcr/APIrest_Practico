a494c4cef4828b0cf8c8975ec219dd1efbeb1490
natjcr <njcollazosr@gmail.com>
2023-05-04 15:41:28 -0500

    new8

--- src/main.js
+++ src/main.js
@@ -20,8 +20,10 @@ const api = axios.create({
     })
   });
   
-  function createMovies(movies, container, lazyLoad = false) {
-    container.innerHTML = '';
+  function createMovies(movies, container, { lazyLoad = false, clean = true } = {}) {
+    if (clean) {
+      container.innerHTML = '';
+    }
   
     movies.forEach(movie => {
       const movieContainer = document.createElement('div');
@@ -115,9 +117,39 @@ const api = axios.create({
     const { data } = await api('trending/movie/day');
     const movies = data.results;
   
-    createMovies(movies, genericSection);
+    createMovies(movies, genericSection, { lazyLoad: true, clean: true });
+
+  /*   const btnLoadMore = document.createElement('button');
+    btnLoadMore.innerHTML = 'Cargar Más';
+    btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+    genericSection.appendChild(btnLoadMore); */
   }
   
+  async function getPaginatedTrendingMovies() {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    
+    const scrollIsButton = (scrollTop + clientHeight) >= (scrollHeight - 15)
+    
+    if(scrollIsButton) {
+        page ++;
+      const { data } = await api('trending/movie/day', {
+        params: {
+          page,
+        }
+      });
+      const movies = data.results;
+    
+      createMovies(movies, genericSection, { lazyLoad: true, clean: false });
+    } 
+  
+  /*   const btnLoadMore = document.createElement('button');
+    btnLoadMore.innerHTML = 'Cargar Más';
+    btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+    genericSection.appendChild(btnLoadMore); */
+  
+  }
+
+
   async function getMovieById(id) {
     const { data: movie } = await api('movie/' + id);
   
