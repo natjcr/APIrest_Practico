8c14121381d279d15bd5efd124d8c9b4033f3947
natjcr <njcollazosr@gmail.com>
2023-05-11 17:07:34 -0500

    api

---
+++ src/API.js
@@ -0,0 +1,11 @@
+const API_KEY = '548b0c6826fd1490a6918d5c4c174eb9';
+
+/* Uso de axion simplificación :
+async function getTrendingMoviesPreview() {
+  const { data } = await api('trending/movie/day');
+  const movies = data.results;
+
+  async function getCategegoriesPreview() {
+  const { data } = await api('genre/movie/list');
+  const categories = data.genres;
+*/
